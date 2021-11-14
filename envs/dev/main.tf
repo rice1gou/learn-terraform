394a4ac4bafb20a663e2762b6ab443b462ed32d7
Oishi Azi <ha1ma2chi3@gmail.com>
2021-11-14 23:27:32 +0900

    first commit

---
+++ envs/dev/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+resource "random_pet" "prefix" {}
+
+resource "azurerm_resource_group" "example" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = "${random_pet.prefix.id}-aks"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  dns_prefix          = "${random_pet.prefix.id}-k8s"
+
+  default_node_pool {
+    name            = "default"
+    node_count      = 2
+    vm_size         = "Standard_D2_v2"
+    os_disk_size_gb = 30
+  }
+  identity {
+    type = "SystemAssigned"
+  }
+}
---
+++ envs/dev/variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group_name" {
+  type    = string
+  default = "my-learning-rg"
+}
+
+variable "location" {
+  type    = string
+  default = "eastus"
+}
